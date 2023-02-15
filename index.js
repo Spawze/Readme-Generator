c12a4c30db5d0f77131775146ccfe37dbadaa4aa
Riley Allred <riley_allred@hotmail.com>
2023-02-15 11:22:30 -0700

    finished most of the code

--- index.js
+++ index.js
@@ -1,13 +1,62 @@
 // TODO: Include packages needed for this application
-
+const fs = require('fs')
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
+const fileName = './output/README.md'
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type: "input",
+    message: "Enter the title of your project: ",
+    name: "title"
+}, {
+    type: "input",
+    message: "Enter the description for your project: ",
+    name: "description"
+}, {
+    type: "input",
+    message: "Enter your installation instructions: ",
+    name: "instructions"
+}, {
+    type: "input",
+    message: "Enter the usage information for your project: ",
+    name: "usage"
+}, {
+    type: "input",
+    message: "Enter the contribution guidelines for your project: ",
+    name: "contribution"
+},{
+    type: "list",
+    message: "Choose a license",
+    name: "license",
+    choices:['MIT','Apache LIcense 2.0','ISC',"No License"]
+},{
+    type: "input",
+    message: "Enter your name for the license",
+    name: "name"
+},
+{
+    type: "input",
+    message: "Enter the year for your license",
+    name: "year"
+},
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+
+
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err =>{
+        if(err) {console.log(err)}
+        else{console.log("Success")}
+    })
+ }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+        .then((data) => {
+            writeToFile(fileName, generateMarkdown(data))
+        })
+}
 
-// Function call to initialize app
-init();
+
+init()
